52d8ad708e3e2ae8c7d6075445d62a076e53a055
dingdaizong0101 <dingdaizong0101@126.com>
2014-02-13 14:36:33 +0800

    GUI

--- gui/src/fdu/kernelpanic/gui/MainActivity.java
+++ gui/src/fdu/kernelpanic/gui/MainActivity.java
@@ -1,15 +1,22 @@
 package fdu.kernelpanic.gui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 public class MainActivity extends Activity {
+
+
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
     }
+
 }
+
+
